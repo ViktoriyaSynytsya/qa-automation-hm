048b6075f4e41373f4d6d67b437276584ac80a8a
Viktoriya Synytsya <viktoriya.synytsya@eleks.com>
2020-04-14 12:23:13 +0300

    js homework

---
+++ hm2/main.js
@@ -0,0 +1,26 @@
+let num1 = 7
+let num2 = 8
+console.log(num1+num2)
+
+let str='my knowledge of JS'
+console.log("Test "+str)
+
+let name = 'Viktoriya '
+let surname = 'Synytsya '
+let age1 = 26
+console.log(name+surname+age1)
+
+let num3=900
+console.log(num3>100)
+
+let num4=900
+let num5=700
+let num6=100
+console.log(num4>(num5&&num6))
+
+let num7=0
+let num8=50
+console.log(num7||num8===50 || num7+num8===50)
+
+let num9=30
+console.log(num9%3==0||num9%7==0)
