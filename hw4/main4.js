8dabf361de1b2513fcaec9e895eb90948c4e1bfd
Viktoriya Synytsya <viktoriya.synytsya@eleks.com>
2020-04-27 19:11:50 +0300

    HW4 changed

--- hw4/main4.js
+++ hw4/main4.js
@@ -1,37 +1,52 @@
 /*Write a JavaScript function to get the last element of an array*/
 
-function ar () {
-    let array = [1,2,3,4,5]
+function ar() {
+    let array = [1, 2, 3, 4, 5]
     let lastItem = array.pop()
-    console.log('Last item = ' + lastItem)}
-    ar()
-	
-/*Write a simple JavaScript program to join all elements of the following array into a string*/
+    console.log('Last item = ' + lastItem)
+}
+ar()
 
-let arr = ['Cat', 'Dog', 'Horse']   
-console.log(arr.join(""))
+/*Write a simple JavaScript program to join all elements of the following array into a string*/
+function arrstring() {
+    let arr = ['Cat', 'Dog', 'Horse']
+    console.log(arr.join(""))
+}
+arrstring()
 
 /*There are two arrays with individual values, write a JavaScript program to compute the sum 
 of each individual index value from the given arrays and save them to third array*/
-let array1 = [1,2,14,100]
-let array2 = [2,5,7,0,3]
-let array3 = [array1[0]+array2[0],array1[1]+array2[1],array1[2]+array2[2],array1[3]+array2[3]]
-console.log('Sum = '+ array3)
 
+let array1 = [1, 2, 14, 100]
+let array2 = [2, 5, 7, 0, 3]
+let array3 = []
+/*let array3 = [array1[0]+array2[0],array1[1]+array2[1],array1[2]+array2[2],array1[3]+array2[3]]
+console.log('Sum = '+ array3)*/
+let length;
+if (array2.length > array1.length) {
+    length = array1.length;
+} else {
+    length = array2.length;
+}
+for (let i = 0; i < length; i++) {
+    array3.push(array1[i] + array2[i])
+}
+console.log(array3)
 /*Write a JavaScript program to calculate the area and perimeter of a rectangle. 
 Rectangle should be an object with properties width and height and methods getArea() and getPerimeter();*/
 
 let rectangle = {
-	width:110,
-    height:220
-	}
-rectangle.getArea = function (){
-return this.height * this.width;
+    width: 110,
+    height: 220,
+    getArea: function () {
+        return this.height * this.width;
+    },
+    getPerimeter: function () {
+        return this.height * 2 + this.width * 2;
+    }
 }
-rectangle.getPerimeter = function () {
-return this.height * 2 + this.width * 2;
-}
-console.log ('Area = '+ rectangle.getArea() + ',' + 'Perimeter = ' + rectangle.getPerimeter())
+
+console.log('Area = ' + rectangle.getArea() + ',' + 'Perimeter = ' + rectangle.getPerimeter())
 
 
 
