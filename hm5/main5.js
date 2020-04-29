40ea9dd9d344de1b62f819086a1740bdd72e65b8
Viktoriya Synytsya <viktoriya.synytsya@eleks.com>
2020-04-29 19:48:52 +0300

    Move classes to separate files

--- hm5/main5.js
+++ hm5/main5.js
@@ -1,56 +1,10 @@
-class Vehicle {
-    constructor(carModel, carYear, maxSpeed) {
-        this.model = carModel;
-        this.year = carYear;
-        this.maxSpeed = maxSpeed;
-    }
-    displayInfo() {
-        console.log(`Name: ${this.model};
-                    Year: ${this.year};
-                    Max Speed: ${this.maxSpeed};`);
-    }
+let Car = require("./car");
+let Truck = require("./truck");
+let Vehicle = require("./vehicle")
 
-    static testCar(car){
-        console.log(`${car.model} works fine!`)
-    }
-}
-class Car extends Vehicle {
-    constructor(carModel, carYear, maxSpeed) {
-        super(carModel, carYear, maxSpeed);
-        this.type = "car";
-    }
+let nissan = new Car('Nissan', 2019, 200);
+let volvo = new Truck('Volvo', 2020, 120);
+nissan.displayInfo();
+volvo.displayInfo();
 
-    transportPeople() {
-        console.log('I am starting transporting passengers');
-    }
-
-    displayInfo() {
-        console.log(`Name: ${this.model};
-                    Year: ${this.year};
-                    Max Speed: ${this.maxSpeed};
-                    Type: ${this.type};`);
-    }
-}
-class Truck extends Vehicle {
-    constructor(carModel, carYear, maxSpeed) {
-        super(carModel, carYear, maxSpeed);
-        this.type = "truck";
-    }
-
-    transportPeople() {
-        console.log('I am starting transporting heavy containers');
-    }
-
-    displayInfo() {
-        console.log(`Name: ${this.model};
-                    Year: ${this.year};
-                    Max Speed: ${this.maxSpeed};
-                    Type: ${this.type};`);
-    }
-}
-let car = new Car('Nissan', 2019, 200);
-let truck = new Truck('Volvo', 2020, 120);
-car.displayInfo();
-truck.displayInfo();
-
-Vehicle.testCar(car);
+Vehicle.testCar(nissan);
