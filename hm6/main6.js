781dadc02aaee17765fc2897c4094e7c566db2a0
Viktoriya Synytsya <viktoriya.synytsya@eleks.com>
2020-05-03 20:26:15 +0300

    Add promise

--- hm6/main6.js
+++ hm6/main6.js
@@ -1,15 +1,43 @@
 
 
-function login(email, pass) {
-    emailInput.sendKeys(email).then(() => {
-        passwordInput.sendKeys(pass).then(() => {
-            loginButton.click()
-        })
-    })
+
+class FieldInput {
+  sendKeys(keys) {
+    return new Promise((resolve, reject) =>
+      setTimeout(() => {
+        console.log('keys sent: ' + keys); resolve(1);
+      }, 2000)
+    )
+  }
 }
 
-async function login(email, pass) {
-    await emailInput.sendKeys(email);
-    await passwordInput.sendKeys(pass);
-    await loginButton.click();
+class Button {
+
+  click() {
+    return new Promise((resolve, reject) =>
+      setTimeout(() => { console.log('click!'); resolve(); }, 2000)
+    )
+  }
 }
+
+let emailInput = new FieldInput();
+let passwordInput = new FieldInput();
+let loginButton = new Button();
+
+function login(email, pass) {
+  emailInput.sendKeys(email).then(() => {
+    passwordInput.sendKeys(pass).then(() => {
+      loginButton.click()
+    })
+  })
+}
+
+async function loginAsync(email, pass) {
+  await emailInput.sendKeys(email);
+  await passwordInput.sendKeys(pass);
+  await loginButton.click();
+}
+
+
+Promise.all([login('viktoriasynytsa@gmail.com', '12345'), loginAsync("aaaqq@mail.com", "65774")])
+  .then((results) => { console.log(results) })
