7c8bd2ee7f806e0ff3558772345b12872033b4e3
Viktoriya Synytsya <viktoriya.synytsya@eleks.com>
2020-05-07 14:59:28 +0300

    file reads added

--- hm7/hw7.js
+++ hm7/hw7.js
@@ -25,6 +25,26 @@ let vika = new Student('Vika', 'b', '3')
 console.log(vika)
 let vikaJson = JSON.stringify(vika);
 console.log(vikaJson);
-fs.writeFile("vika.txt", vikaJson, function(err){
-    if(err) throw err;
+
+fs.writeFile("vika.txt", vikaJson, function (err) {
+    if (err) throw err;
 })
+
+fs.readFile("vika.txt", "utf8", function (err, data) {
+    if (err) throw err;
+    var jsonFromFile = JSON.parse(data)
+    jsonFromFile._name = "Kolya";
+    fs.appendFile("vika.txt", JSON.stringify(jsonFromFile), function (err) {
+        if (err) throw err;
+    })
+    fs.readFile("vika.txt", "utf8", function (err, data) {
+        if (err) throw err;      
+        console.log(data)
+    })
+})
+// setTimeout(()=>{
+//     fs.readFile("vika.txt", "utf8", function (err, data) {
+//         if (err) throw err;      
+//         console.log(data)
+//     })
+// }, 5000)
