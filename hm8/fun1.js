0721ca354a9b978839b8faab44bc4ceabfc9daf2
Viktoriya Synytsya <viktoriya.synytsya@eleks.com>
2020-05-19 20:16:06 +0300

    first test added

--- hm8/fun1.js
+++ hm8/fun1.js
@@ -1,6 +1,4 @@
-function sum(x, y) {
-    return x+y;
-    
-}
+module.exports = function sum(x, y) {
+    return x + y;
 
-console.log(sum(1,8))
+}
--- hm8/spec/testspec1.js
+++ hm8/spec/testspec1.js
@@ -1,21 +1,7 @@
+let sum = require("../fun1.js")
 
 describe("Hello world", function () {
-  it("Hello test", function () { 
-    expect(true).toBe(false)
+  it("Hello test", function () {
+    expect(sum(1, 2)).toBe(3)
   })
 })
-
-// let funSpec1 = require("/hm8/fun1.js")
-// let Jasmine = require('jasmine');
-// let jasmine = new Jasmine();
-
-// // console.log(sum(1,2))
-
-// describe("My custom test", function() {
-//      it("should work", function() {
-//          let result = funSpec1;
-//          expect(result).toBeSumOf(7);
-//      });
-//    });
-
-//   //jasmine.execute('testspec1.js')
